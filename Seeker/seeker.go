69a7138ccf4c24723acaede1d749f9a61b31bf1c
Ryan Prairie <me@ryanprairie.com>
2020-10-30 16:27:30 -0400

    removed unused stuff from before

--- Seeker/seeker.go
+++ Seeker/seeker.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"bufio"
-	"container/list"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"os"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/Knetic/govaluate"
@@ -17,13 +15,13 @@ import (
 func main() {
 	// read port from console
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter address to connect to:")
+	fmt.Print("Enter address to connect to: ")
 	addr, _ := reader.ReadString('\n')
 
-	fmt.Print("Enter port to connect to:")
+	fmt.Print("Enter port to connect to: ")
 	port, _ := reader.ReadString('\n')
 	// add the address and get rid of the \n at the end
-	fullAddr := addr[:len(addr)-1] + port[:len(port)-1]
+	fullAddr := addr[:len(addr)-1] + ":" + port[:len(port)-1]
 	fmt.Println(addr)
 
 	// set timeout and connection
@@ -65,106 +63,14 @@ func main() {
 			if err != nil {
 				conn.Write([]byte("JOB FAIL"))
 			}
-			conn.Write([]byte("DONE EQ " + expResult.(string)))
-		}
-	}
-}
-
-func handleConnection(conn net.Conn, mutex *sync.Mutex, queue *list.List) {
-	// state values
-	// 0 = connection initalized
-	// 1 connection established
-	// 2 full
-	// 3 ready for jobs
-	// 4 closed
-	state := 0
-	currSession := IdvSession{
-		// session ID is gonna be the time in nano seconds
-		// I thought bout writing a mathematically unique ID
-		// but the chances are crazy low soooooooo I aint doing it
-		time.Now().UnixNano(),
-		state,
-		conn,
-	}
-	mutex.Lock()
-	queue.PushBack(currSession)
-	mutex.Unlock()
-	// event loop
-	for {
-		result, err := ioutil.ReadAll(conn)
-		if err != nil {
-			closeConnection(conn)
+			conn.Write([]byte("JOB SUCC " + expResult.(string)))
 			state = 4
 		}
-
-		cleanedResult := strings.TrimSpace(string(result))
-
-		// set state based on position
-		position, entry := getPosition(&currSession, mutex, queue)
-		if position == 0 {
-			state = 3
-		} else {
-			state = 2
-		}
-
 		if state == 4 {
-			mutex.Lock()
-			queue.Remove(entry)
-			mutex.Unlock()
-		}
-
-		// initial message handling
-		if state == 0 && strings.Compare(cleanedResult, "HELLO") == 0 {
-			state = 1
-			conn.Write([]byte("HELLOACK"))
-		} else if state == 2 { // options for when its not at front of queue
-			if strings.Compare(cleanedResult, "AVL") == 0 {
-				conn.Write([]byte("FULL"))
-			}
-		} else if state == 3 { // options for when it IS at the front of the queue
-			if strings.Compare(cleanedResult, "AVL") == 0 {
-				conn.Write([]byte("AVLACK"))
-			} else if strings.Compare(cleanedResult, "JOB TIME") == 0 {
-				daytime := time.Now().String()
-				conn.Write([]byte("DONE TIME " + daytime))
-			} else if strings.Compare(cleanedResult[:6], "JOB EQ") == 0 { // evaluate equation
-				expression, _ := govaluate.NewEvaluableExpression(cleanedResult[6:])
-				expResult, _ := expression.Evaluate(nil)
-				conn.Write([]byte("DONE EQ " + expResult.(string)))
-			}
-		} else if strings.Compare(cleanedResult, "BYE") == 0 {
-			state = 4
-			continue
-		}
-
-		if state == 4 {
+			conn.Close()
 			break
 		}
 	}
-
-}
-
-func getPosition(currSession *IdvSession, mutex *sync.Mutex, queue *list.List) (int, *list.Element) {
-	mutex.Lock()
-	// Iterate through list and print its contents.
-	// ya ya ya i know it could be a binary search but i dont have enough time to write that
-	position := 0
-	var e *list.Element
-	for e, index := queue.Front(), 0; e != nil; e, index = e.Next(), index+1 {
-		fmt.Println(e.Value)
-		// checking the value of the current session to the session in queue
-		if currSession.id == e.Value.(*IdvSession).id {
-			position = index
-			break
-		}
-	}
-	mutex.Unlock()
-	return position, e
-}
-
-func closeConnection(conn net.Conn) {
-	conn.Write([]byte("BYE"))
-	conn.Close()
 }
 
 func checkError(err error) {
