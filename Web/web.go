e969fb70cc064ddda6f49699edfc0c7011f22e30
Ryan Prairie <me@ryanprairie.com>
2020-12-22 14:20:16 -0500

    queue JSON API is complete

--- Web/web.go
+++ Web/web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -21,15 +22,47 @@ type Server struct {
 	job       chan string
 }
 
+// go handler for queue GET request
+// method: GET
+// output:
+// ```
+// {
+// queue: [
+// 		"123.321.123.0",
+// 		"123.321.323.0",
+// 		"123.321.333.0",
+//		]
+//	}
+// ```
 func (s *Server) queueHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("URL is: ", r.URL.Path)
+	// if method is GET
+	if r.Method == http.MethodGet {
 
-	// send the message to write to it
-	s.queueTR <- 1
+		// send the message to write to it
+		s.queueTR <- 1
 
-	// recieves the queue
-	conn := <-s.queueRV
-	fmt.Fprint(w, "first conn is %s", conn[0].RemoteAddr().String())
+		// recieves the queue
+		connQueue := <-s.queueRV
+
+		// just init stuff
+		var queueJson map[string][]string
+		queueJson = make(map[string][]string)
+		queueJson["queue"] = make([]string, 0)
+
+		// add each ip to the map
+		for _, indvConn := range connQueue {
+			queueJson["queue"] = append(queueJson["queue"], indvConn.RemoteAddr().String())
+		}
+
+		w.WriteHeader(http.StatusAccepted)
+		// changes the map to json
+		// writes the json to the writer
+		json.NewEncoder(w).Encode(queueJson)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("400 - Wrong request method"))
+	}
+
 }
 
 func (s *Server) jobHandler(w http.ResponseWriter, r *http.Request) {
